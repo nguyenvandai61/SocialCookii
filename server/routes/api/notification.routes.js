5e51ab5ed5f992da3c56c60612ac6da4138a71f6
okuloj <okuloj>
2020-11-18 09:34:48 +0700

    update notification-routes API

--- server/routes/api/notification.routes.js
+++ server/routes/api/notification.routes.js
@@ -9,4 +9,16 @@ router.post('/', (req, res) => {
         return notificationController.createNotifications(req, res)
     return notificationController.createNotification(req, res)
 })
+
+router.get('/', (req, res)=> notificationController.getNotification(req, res))
+
+router.put('/', (req, res)=> notificationController.updateNotification)
+
+router.delete('/', (req, res)=> {
+    // Neu req.body rong thi xoa het
+    if (Object.keys(req.query).length == 0)    
+        return notificationController.deleteAllNotifications(req, res);  
+    return notificationController.deleteNotification(req, res)
+})
+
 module.exports = router
