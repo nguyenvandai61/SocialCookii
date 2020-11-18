1ac0ed9e62927bcf066a39d6656f485cc179a14e
okuloj <okuloj>
2020-11-18 15:56:03 +0700

    add fetch API AddPost

--- client/src/components/AddPost/AddPost.js
+++ client/src/components/AddPost/AddPost.js
@@ -7,6 +7,49 @@ import Avatar from '../../avatar.jpg';
 class AddPost extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            post: {
+                title: "Mi xao",
+                description: "Nguyen lieu: Mi + Trung ga",
+                authorId: "5fa7edd8869c07126058a867",
+                createdAt: "2020-11-08T01:11:18.965Z",
+                deletedAt: "",
+                editedAt: "",
+                thumbnail: {
+                    url: "/images/mongodb.png",
+                    caption: "cong thuc"
+                },
+                album: [
+                    {
+                        url: "/images/anh1.png",
+                        caption: "cong thuc"
+                    },
+                    {
+                        url: "/images/anh2.png",
+                        caption: "ket qua"
+                    }
+                ],
+                videos: [
+                    {
+                        url: "/images/video.mp3",
+                        caption: "cach lam"
+                    }
+                ],
+                comments: [
+                    "5fa7f3aa108cb725f035f21a",
+                    "5fa7efe5869c07126058a868"
+                ],
+                likeUserIds: [
+                    "5fa7f3aa108cb725f035f21a",
+                    "5fa7efe5869c07126058a868"
+                ],
+                state: "Da dang",
+                hashtagIds: "5fa75587997be636b8f92cb3"
+            }
+        };
+        this.onAddImages = this.onAddImages.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
     }
     componentWillMount() {
         const script = document.createElement("script");
@@ -40,6 +83,50 @@ class AddPost extends Component {
     componentWillUnmount() {
 
     }
+    onSubmit(event) {
+        event.preventDefault();
+        console.log('clicked Post');
+        const {
+            title,
+            description,
+            authorId,
+            createdAt,
+            deletedAt,
+            editedAt,
+            thumbnail,
+            album,
+            videos,
+            comments,
+            likeUserIds,
+            state,
+            hashtagIds,
+        } = this.state.post;
+        fetch('api/post', {
+            method: "POST",
+            body: JSON.stringify({
+                title: title,
+                description: description,
+                authorId: authorId,
+                createdAt: createdAt,
+                deletedAt: deletedAt,
+                editedAt: editedAt,
+                thumbnail: thumbnail,
+                album: album,
+                videos: videos,
+                comments: comments,
+                likeUserIds: likeUserIds,
+                state: state,
+                hashtagIds: hashtagIds
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log('Success:', result);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
     render() {
         return (
             <div class="new-post">
@@ -70,6 +157,6 @@ class AddPost extends Component {
 }
 AddPost.propTypes = {
 
-                    };
+};
 
 export default AddPost;
