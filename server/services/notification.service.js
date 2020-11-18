2cbbca0ddf948ec724a0f3dfc06773a62027e0a7
okuloj <okuloj>
2020-11-18 09:33:18 +0700

    update notification-service

--- server/services/notification.service.js
+++ server/services/notification.service.js
@@ -9,6 +9,47 @@ const createNotification = (res, notification) => {
     });
 }
 
+const getNotification = (req, res) => {
+    Notification.find(req.query, (err, data) => {
+        if (err) return res.status(500).send(err);
+        res.json({ "data": data });
+    })
+}
+
+const updateNotification = (res, query, newData) => {
+    const content = req.body;
+    Notification.findOneAndUpdate(content.query, content.newData, function (err) {
+        if (err) return res.status(500).send(err);
+        const response = {
+            message: "Notification successfully updated"
+        };
+        return res.status(200).send(response);
+    });
+}
+const deleteNotification = (res, query) => {
+    Notification.deleteOne(query, (err, data) => {
+        if (err) return res.status(500).send(err);
+        const response = {
+            message: "Notification successfully deleted"
+        };
+        return res.status(200).send(response);
+    })
+}
+
+const deleteAllNotifications = (req, res) => {
+    Notification.deleteMany({}, (err, data) => {
+        if (err) return res.status(500).send(err);
+        const response = {
+            message: "All questions successfully deleted"
+        };
+        return res.status(200).send(response);
+    })
+}
+
 module.exports = {
     createNotification,
+    getNotification,
+    updateNotification,
+    deleteNotification,
+    deleteAllNotifications,
 }
