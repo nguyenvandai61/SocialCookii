f4a1d76f7336a7aa068b59b1e98e21d2bff53548
okuloj <okuloj>
2020-11-18 09:55:35 +0700

    update friend.controller

--- server/controller/friend.controller.js
+++ server/controller/friend.controller.js
@@ -5,6 +5,18 @@ const createFriend = (req, res) => {
     return friendService.createFriend(res, Friend);
 }
 
+const updateFriend = (req, res) => {
+    let content = req.body;
+    return FriendService.updateFriend(res, content.query, content.newContent);
+}
+
+const getFriend = (req, res) => {
+    return FriendService.getFriend(req, res);
+}
+
+
 module.exports = {
     createFriend,
+    updateFriend,
+    getFriend,
 }
