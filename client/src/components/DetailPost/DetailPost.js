e6827c83775cd34d58b98be157aecac02adb52bd
thucong <51808157+thucong@users.noreply.github.com>
2020-11-27 09:50:30 +0700

    edit personalInfo

--- client/src/components/DetailPost/DetailPost.js
+++ client/src/components/DetailPost/DetailPost.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import './DetailPost.css';
 class DetailPost extends Component {
     constructor(props) {
         super(props);
@@ -37,88 +37,77 @@ class DetailPost extends Component {
 
     render() {
         return (
-            <div>
-                <h1 align="center">Bánh xèo</h1>
-                <img src="../src/image/dishes/banhxeo.png" alt="" height="300px" width="600px" /> 
-                <br/>
+            <div  class="content">
+            <div class="left">
+                <img src="../src/avatar.jpg" alt="" height="" width="" />
+
+            </div>
+            <div class="right">
+                <h1>Bánh xèo </h1>
+                <p>Bánh xèo là một món ăn truyền thống thuần túy và rất quen thuộc đối với chúng ta.
+                    Tuy nhiên ngày nay, bánh xèo Việt Nam đã trở thành một cái tên đặc biệt.
+                    Luôn luôn được nhắc đến bởi nhiều người nước ngoài khi ghé thăm Việt Nam. 
+                    Bánh xèo cũng được biến tấu nhiều phù hợp với khẩu vị, phong tục của từng địa phương khác nhau. 
+                    Nhưng đều giữ chung cho món ăn này một hương vị riêng. Để lại cho người thưởng thức nhiều cảm xúc khó quên khi dùng qua dù chỉ là một lần.
+                </p>
+                <div class="info">
+                    <div class="col-sm-9">
+                        <img src="../src/avatar.jpg" alt="" height="60px" width="60px" class="avatar"/>
+                        <h2>YourName</h2>
+                    </div>
+                    <div class="col-sm-3">
+                        <input type="submit" value="Theo dõi" />
+                    </div>
+                    
+                </div>
                 <div>
                     <i class="far fa-heart"></i> Like  &ensp;
                     <span>10 Lượt like</span>
-                </div>
-                <p>Bánh xèo là một món ăn truyền thống thuần túy và rất quen thuộc đối với chúng ta.
-                        Tuy nhiên ngày nay, bánh xèo Việt Nam đã trở thành một cái tên đặc biệt.
-                        Luôn luôn được nhắc đến bởi nhiều người nước ngoài khi ghé thăm Việt Nam. 
-                        Bánh xèo cũng được biến tấu nhiều phù hợp với khẩu vị, phong tục của từng địa phương khác nhau. 
-                        Nhưng đều giữ chung cho món ăn này một hương vị riêng. Để lại cho người thưởng thức nhiều cảm xúc khó quên khi dùng qua dù chỉ là một lần.
-                </p>
-                <div>
-                    <h2>Cách làm bánh xèo đơn giản – Chuẩn bị nguyên liệu đơn giản</h2>
-                    <p>
-                        Đầu tiên cho bất kỳ món ăn nào đó chính là khâu chuẩn bị nguyên vật liệu.
-                            Như đã nói ở trên, đây sẽ là cách làm bánh xèo đơn giản nhất, Vì vậy, các nguyên liệu cần có cũng vô cùng đơn giản. 
-                            Bạn chỉ cần chuẩn bị: </p>
-                    <ul>
-                        <li> Bột bánh xèo Mikko : 500g</li>
-                        <li>Tôm tươi: 150g</li>
-                        <li>Thịt ba chỉ heo: 150g</li>
-                        <li>Nấm hương: 15 cái</li>
-                        <li>Giá đỗ: 100g</li>
-                        <li>Đỗ sạch đã bóc vỏ: 50g</li>
-                        <li>Các loại nguyên liệu khác như: hành lá, cà rốt, xà lách, rau thơm, dầu ăn, đường, mắm, tiêu, hạt nêm ……</li>
-                    </ul>
-                    <img src="../src/image/dishes/chebien_bx.png" alt="" height="200px" width="400px"/>
-                </div>
-                <h2>Các bước làm bánh xèo đơn giản:</h2>
-                <div>
-                    <h3>Bước 1: Sơ chế nguyên vật liệu để đổ bánh xèo</h3>
-                    <ul>
-                        <li>Đầu tiên, bạn tiến hành bóc bỏ các vỏ hành, tỏi, rửa sạch tất cả rồi thái chúng thành từng miếng nhỏ vừa phải.</li>
-                        <li>Cắt bỏ đầu, chân, và đuôi tôm. Lưu ý bạn cần lấy sạch các gân đen của tôm để tránh để lại mùi tanh. Sau đó rửa sạch,
-                                để ráo nước rồi ướp với các gia vị như muối, gừng, tỏi…</li>
-                        <li>Rửa sạch thịt ba chỉ rồi thái thành miếng mỏng. Ướp hạt nêm và tỏi băm sao cho  vừa ăn với khẩu vị của bạn và gia đình.</li>
-                        <li>Nấm hương cho vào nước ngâm đến khi nấm nở, cắt bỏ chân nấm rồi thái thành sợi mỏng.</li>
-                        <li>Cà rốt cũng thái thành từng sợi mỏng, để sạch.</li>
-                        <li>Các loại rau dùng để ăn sống thì nhặt sạch sẽ, loại bỏ các lá sâu úa, ngâm với nước muối rồi để ráo nước.</li>
-                    </ul>
-                    <img src="../src/image/dishes/nguyenlieu_bx.png" alt="" height="200px" width="400px"/>
-                </div>
-                <div>
-                    <h3>Bước 2: cách pha bột làm bánh xèo đơn giản nhất:</h3>
-                    <p>Đối với cách pha bột bánh xèo Mikko này bạn có thể tiết kiệm được nhiều thời gian hơn trong khâu chuẩn bị cũng như chế biến. Bởi vì trong gói bột pha sẵn Mikko đã có đầy đủ các nguyên liệu cần thiết cho chúng ta chế biến. 
-                        <br/>
-                        Bạn không cần thêm nghệ, vì trong bột bánh xèo Mikko đã có sẵn bột nghệ. Tạo một màu vàng vừa phải, tự nhiên. Bên cạnh đó để tăng thêm độ ngon cho món ăn, bạn có thể thêm 1 muỗng dầu ăn vào bột. Sau đó khuấy đều. Lưu ý, đổ từ từ nước lọc vào bột, khuấy đều tay để bột tan hết, không bị vón cục.
-                        <br/>
-                        Sau đó, bạn cho một ít hành lá đã được thái nhỏ vào bột, khuấy đều. Để bột  nghỉ từ 15 đến 20 phút là bạn có thể tiến hành đổ bánh xèo.</p>
-                        <img src="../src/image/dishes/bột-bánh-xèo.png" alt="" height="200px" width="400px"/>
-                </div>
-                <div>
-                    <h3>Bước 3: Làm nhân bánh xèo</h3>
-                    <p>Trong thời gian chờ bột nghỉ, chúng ta hãy bắt tay vào làm nhân bánh xèo. Bạn bắt chảo lên bếp, sau đó đun nóng chảo với một ít dầu ăn. Đến khi cảm thấy được dầu đã nóng vừa phải thì lần lượt cho thịt, nấm hương, tôm và xào đều. Lưu ý không nên để dầu quá nóng vì khi ấy sẽ làm mất chất của các nguyên vật liệu làm nhân bánh xèo. 
-                        <br/>
-                        Trộn đều tay, đến khi tất cả tái chín thì tiến hành nêm nếm sao cho vừa ăn với khẩu vị của bạn mong muốn. Đợi thêm vài phút để tất cả gia vị đã thấm đều vào nhân thì bắt chảo xuống và cho tất cả ra bát để nguội.</p>
-                    <img src="../src/image/dishes/nhan_bx.png" alt="" height="200px" width="400px"/>
-                </div>
-                <div>
-                    <h3>Bước 4: Cách đổ bánh xèo đơn giản nhất:</h3>
-                    <p>Sau khi đã kết thúc tất cả các bước sơ chế nguyên liệu cũng như chuẩn bị xong nhân bánh. Chúng ta đã đầy đủ các thứ cần thiết để đổ bánh xèo. 
-                        <br/>
-                        Một lưu ý trong cách làm bánh xèo đơn giản đó chính là lựa chọn khuôn đổ bánh. Chúng ta nên lựa chọn khuôn hay chảo mỏng, có lòng chảo sâu. Thì lúc đổ bánh, bánh xèo sẽ mỏng và đều, đẹp hơn.
-                        <br/>
-                        Bắt đầu cho chảo lên bếp. Nhỏ vào một ít dầu ăn, đun nóng. Khi chảo nóng, múc khoảng 1 muỗng canh nhỏ bột đã pha sẵn đổ đều khắp lòng chảo. Đậy nắp và đợi trong vài phút. 
-                        <br/>
-                        Sau đó bạn mở nắp chảo ra, cho tiếp phần nhân bánh đã chuẩn bị trước đó vào trên bề mặt bánh. Bỏ thêm một ít giá đỗ và sợ cà rốt để tạo màu sắc hấp dẫn hơn. Đậy nắp chảo và đợi khoảng 2 phút, lúc này bánh xèo đã hoàn toàn chín.
-                        <br/>
-                        Cuối cùng, bạn mở nắp chảo ra. Tiến hành quan sát vành bánh. Nếu thấy vành bánh đã giòn thì tiến hành gấp đôi bánh lại. Cẩn thận khi gấp để bánh xèo không bị nát. Sắp ngay ngắn ra đĩa.</p>
-                    <img src="../src/image/dishes/do-banh-xeo.png" alt="" height="200px" width="400px"/>
-                </div>
-                <div>
-                    <h3>Bước 5: cách làm nước mắm ăn bánh xèo đơn giản nhất:</h3>
-                    <p>Mikko chia sẻ cho bạn một trong những cách pha nước mắm vô cùng đơn giản, dễ làm mà lại rất thơm ngon. Bạn hãy trộn đều hợp hợp gồm: 3 thìa nước mắm, ½ thìa nước sôi, 1 muỗng đường, 1 thìa tỏi băm và 2 trái ớt chín đỏ đã được băm nhuyễn. Khuấy đều cho đến khi đường tan hết, cho thêm một ít nước cốt chanh vào. Trộn đều lên,bạn sẽ có được chén mắm chấm thơm ngon, đậm đà. </p>
-                    <img src="../src/image/dishes/nuoc-mam.png" alt="" height="200px" width="400px"/>
-                </div>
-                <p>Hy vọng với những chia sẻ về cách làm bánh xèo đơn giản nhất mà Mikko đã chia sẻ ở trên sẽ giúp bạn có thể tự làm được món ăn đặc biệt này ngay tại căn bếp nhỏ của mình.</p>
+                </div> <br/>
+                <h3>Nguyên liệu</h3>
+                <div class="content1">
+                    <div>
+                        <ul>
+                            <li> Bột bánh xèo Mikko : 500g</li>
+                             <li>Tôm tươi: 150g</li>
+                            <li>Thịt ba chỉ heo: 150g</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Nấm hương: 15 cái</li>
+                            <li>Giá đỗ: 100g</li>
+                            <li>Đỗ sạch đã bóc vỏ: 50g</li>
+                        </ul>
 
+                    </div>
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <form>
+                        <div class="cmt">
+                            <img src="../src/avatar.jpg" alt="" height="60px" width="60px" class="avatar" />
+                            <input type="text" placeholder="Thêm nhận xét" /> <br/>
+                        </div> <br/>
+                        <div class="submit" align="center" >
+                            <input type="button" value="Hủy" />
+                            <input type="submit" value="OK"/>
+                        </div>
+                        
+                    </form>
+                </div>
+                <div>
+                    <div class="cmt">
+                        <img src="../src/avatar.jpg" alt="" height="60px" width="60px" class="avatar" />
+                        <div class="ct">
+                            <h5>YourName</h5>
+                            <p>Bài viết hay</p>
+                        </div>
+                    </div> <br/>
+                    
+                </div>
             </div>
+        </div>
         );
     }
 }
