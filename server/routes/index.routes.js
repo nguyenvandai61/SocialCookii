05e35f1a672fdef54405f0a6f77d2d4ce6a233cd
nguyenvandai61 <crystallifestyle05@gmail.com>
2020-12-05 15:44:20 +0700

    Detail post GUI

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var path = require('path')
 var router = express.Router();
-let fileLocation = path.join(__dirname, '..','assets/image/posts', '1.png');
-console.log(fileLocation);
+// let fileLocation = path.join(__dirname, '..','assets/image/posts', '1.png');
+// console.log(fileLocation);
+router.get('/image/default/avatar/:file', (req,res) =>{
+  let file = req.params.file;
+  let fileLocation = path.join(__dirname, '..','assets/image/default/avatar', file);
+  console.log("lala");
+  res.sendFile(fileLocation);
+})
 /* GET home page. */
 router.get('/image/posts/:file', (req,res) =>{
     let file = req.params.file;
@@ -10,4 +16,5 @@ router.get('/image/posts/:file', (req,res) =>{
     console.log("lala");
     res.sendFile(fileLocation);
   });
+  
 module.exports = router;
