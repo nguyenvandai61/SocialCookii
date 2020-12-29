172a5163eb8172479212a4960ee1f406a416b05f
nguyenvandai61 <crystallifestyle05@gmail.com>
2020-12-29 21:10:15 +0700

    Prepare deploy heroku

--- server/config.js
+++ server/config.js
@@ -1,12 +1,11 @@
 const JWTSECRET = process.env.JWTSECRET;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-
+const pass = "qk0K2M2nlYAlqTrq";
 module.exports = {
     mongoose: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/cookii'
+      uri: process.env.MONGODB_URI || 'mongodb+srv://teamanmy:qk0K2M2nlYAlqTrq@cluster0.jojjg.mongodb.net/cookii'
     },
     
-    jwtSecret: JWTSECRET,
-    mongodburi: 'mongodb://' + DB_USERNAME + ':' + DB_PASSWORD + '@ds233763.mlab.com:33763/basic-mern-stack-app'
+    jwtSecret: JWTSECRET
 }
