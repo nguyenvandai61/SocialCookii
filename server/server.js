f939bccd87e667081beb341d9381dce0b8e5103b
nguyenvandai61 <crystallifestyle05@gmail.com>
2020-12-29 23:04:39 +0700

    Deploy

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ mongoose.connect(config.mongoose.uri, {
         console.log('Error connecting to database');
     });
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
         res.sendFile(path.resolve(__dirname, '../client/build/', 'index.html'));
 });
 app.use('/*', express.static(__dirname+"../client/build"));
