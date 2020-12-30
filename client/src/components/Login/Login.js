8f287a3d1eafdf342ccc9894b80072c4c4ad24a4
nguyenvandai61 <crystallifestyle05@gmail.com>
2020-12-30 07:37:53 +0700

    Fix login with user

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -53,7 +53,7 @@ class Login extends Component {
         }
         else {
             
-            // window.location.href = "/";
+            window.location.href = "/";
             // this.props.history.push("/");
         }
     }
